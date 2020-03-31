d727cb6b50d01565986b2ee5d5c42c6a5170c132
Jan Katins <jan.katins@zenjob.com>
2020-03-31 12:04:36 +0200

    Version 0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_long_description():
 
 setup(
     name='mara-google-sheet-downloader',
-    version='2.7.0',
+    version='0.1.0',
 
     description='Opinionated lightweight ETL pipeline framework',
 
